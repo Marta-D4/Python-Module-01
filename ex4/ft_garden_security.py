a610bc7b947be824a6040cc1881afbf3135a73f5
Blanditto <78723668+Marta-D4@users.noreply.github.com>
2026-01-14 12:23:18 +0100

    Correction

--- ex4/ft_garden_security.py
+++ ex4/ft_garden_security.py
@@ -5,24 +5,31 @@ class SecurePlant:
         self.name = name.capitalize()
         self.__height = height
         self.__days = days
+        print(f"Plant created : {self.name}")
 
     def get_height(self):
         return self.__height
-    
+
     def get_age(self):
         return self.__days
-    
+
     def get_info(self):
-        return f"Current plant: {self.name} ({self.get_height()}cm, {self.get_age()} days)"
-    
+        return (
+            f"Current plant: {self.name} "
+            f"({self.get_height()}cm, {self.get_age()} days)"
+        )
+
     def set_height(self, height):
         if height < 0:
-            print(f"\nInvalid operation attempted: height {height}cm [REJECTED]")
+            print(
+                "\nInvalid operation attempted: "
+                f"height {height}cm [REJECTED]"
+            )
             print("Security: Negative height rejected")
         else:
             self.__height = height
             print(f"Height updated: {self.__height}cm [OK]")
-    
+
     def set_age(self, days):
         if days < 0:
             print(f"\nInvalid operation attempted: age {days} days [REJECTED]")
